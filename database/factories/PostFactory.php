0431a88ab78aba0d3d22e185d54a15a9bbd8fbcf
Umang Shrestha <sumang20@tbc.edu.np>
2022-12-01 20:18:26 +0545

    PostFactory.php

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->name(),
+            'excerpt' => $this->faker->sentence(),
+            'body' => $this->faker->paragraph(5),
+            'published_at' => now()
+        ];
+    }
+}
